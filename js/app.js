06aa013c227a9ddea93e9bd2c6a55cdf9ee52bef
vanikrishnan <vanijeya1528@gmail.com>
2018-06-07 12:10:06 -0700

    Files Added

--- js/app.js
+++ js/app.js
@@ -2,9 +2,46 @@ function addDetails() {
     console.log('Add button clicked');
    
     if (validateFirstName()) {
-        if (validateFullName()) {
-            // Do a Post
-            postData();
+    if (validateFullName()) {
+    if(validateDesignation()){
+if(validateempcode()){
+if(validatebg()){
+if(validatereason()){
+if(validatemailid()){
+if(validatephno()){
+if(validatecontact()){
+    if(validatedate()){
+        postData();
+    }else{
+        displayErrorMessageForDate();
+    }
+
+}
+else{
+    displayErrorMessageForContact();
+}
+}else{
+    displayErrorMessageForPhone();
+}
+}
+else{
+    displayErrorMessageForMailid();
+}
+}
+else{
+    displayErrorMessageForReason();
+}
+}
+else{
+    displayErrorMessageForBG();
+}
+}else{
+    displayErrorMessageForEmpCode();
+}
+            }
+            else{
+                displayErrorMessageForDesig();
+            }
         }
         else {
             // Display error message for full Name
@@ -12,7 +49,8 @@ function addDetails() {
         
             displayErrorMessageForFullName();
         }
-    } else {
+    }
+     else {
         // Display error message for first Name
         console.log("hii");
         displayErrorMessageForFirstName();
@@ -28,39 +66,150 @@ function displayErrorMessageForFullName() {
     $('#errForFullName').show();
 }
 
+function displayErrorMessageForContact(){
+    $('#errForcontactno').show();
+}
+
+function displayErrorMessageForPhone(){
+    $('#errForphoneno').show();
+}
+
+function displayErrorMessageForDate(){
+    $('#errForDateOfEmployment').show();
+}
+
+function displayErrorMessageForMailid(){
+    $('#errFormailid').show();
+}
+
+function displayErrorMessageForReason(){
+    $('#errForReason').show();
+}
+
+function displayErrorMessageForBG(){
+    $('#errForBloodgroup').show();
+}
+
+function displayErrorMessageForEmpCode(){
+    $('#errForEmployeecode').show();
+}
+
+function displayErrorMessageForDesig(){
+    $('#errForDesignation').show();
+}
 
 function validateFirstName() {
     //var a='#'firstName;
     const firstName = $('#firstName').val();
-    if (firstName === ''  || !fullName.match('/^[a-zA-Z]+$/')) {
+    if (firstName === '') {
         return false;
     }
+    else if(!(isNaN(firstName)))
+    alert("Enter valid firstname");
+    else
     return true;
 }
 
 function validateFullName() {
     const fullName = $('#fullName').val();
-    if (fullName === '' || !fullName.match('/^[a-zA-Z]+$/')) {
+    if (fullName === '' ) {
         return false;
     }
+    else if(!(isNaN(fullName)))
+    alert("Enter valid fullname");
+    else
     return true;
 }
+
+
 
 function validateDesignation() {
     var a='#designation';
     const desig = $(a).val();
-    if (desig === '' || !fullName.match('/^[a-zA-Z]+$/')) {
+    if (desig === '') {
         return false;
     }
+    else if(!(isNaN(desig)))
+alert("Enter valid designation");
+else
     return true;
 }
 
+function validatedate(){
+    const b=$('#dateofemployment').val();
+    if(b=== '')
+    return false;
+
+else
+return true;
+}
+
+function validateempcode(){
+    const b=$('#employeecode').val();
+    const d= /^\d{7}$/;
+    if(b=== '')
+    return false;
+    else if(isNaN(b)  || !(d.test(b)))
+    alert("enter number with 7 digis");
+ 
+else
+return true;
+}
+function validatebg(){
+    const a=$('#bloodgroup').val();
+    const r=/^(A|B|AB|O)[+-]$/;
+if(a==='')
+return false;
+else if(!(r.test(a)))
+    alert("enter valid blood group");
+    else
+    return true;
+}
+
+function validatereason(){
+    const b=$('#re').val();
+    if(b==='')
+    return false;
+else if(b=="None")
+alert("select any one option");
+else
+return true;
+}
+
+
 function validatemailid(){
     const id=$('#mailid').val();
-const a=/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-if(a.test(id))
-    return true;
+const a=/^([a-zA-Z0-9_\.\-])+\@virtusa.com+$/;
+if(id==='')
 return false;
+
+else if(!(a.test(id)))
+    alert("enter valid mailid");
+    else
+return true;
+}
+
+function validatephno(){
+    const b=$('#phoneno').val();
+    const d= /^\d{10}$/;
+    if(b==='')
+    return false;
+    else if(isNaN(b)  || !(d.test(b)))
+    alert("enter number with 10 digis");
+else
+return true;
+}
+
+
+function validatecontact(){
+    const b=$('#contactno').val();
+    const d= /^\d{10}$/;
+    if(b==='')
+    return false;
+else if(isNaN(b) || !(d.test(b)))
+alert("enter number with 10 digis");
+else
+return true;
 }
 
 
@@ -69,11 +218,19 @@ return false;
 function postData() {
     const data = {
         firstName: $('#firstName').val(),
-        fullName: $('#fullName').val()
+        fullName: $('#fullName').val(),
+        designation:$('#designation').val(),
+        employeecode:$('#employeecode').val(),
+        bloodgroup:$('#bloodgroup').val(),
+        reason:$('#re').val(),
+        dateofemployment:$('#dateofemployment').val(),
+        emailid:$('#mailid').val(),
+        phoneno:$('#phoneno').val(),
+        contactno:$('#contactno').val()
     };
     $.ajax({
         type: "POST",
-        url: 'https://id-application-form.firebaseio.com/validationform.json',
+        url: 'https://validationform-5764f.firebaseio.com/validationform.json',
         data: JSON.stringify(data),
         success: onPostSuccess,
         // dataType: dataType
